062ec49084d4b8b01d303025051a394f18f8e486
Tony Wang <tonywang@lbl.gov>
2014-09-06 11:11:42 -0700

    changed index.html to php and moved it outside into the root

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <!-- Javascript Files -->
+        <script type="text/javascript" src="../static/js/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="../static/js/bootstrap.min.js"></script>
+
+        <!-- CSS Files -->
+        <link rel="stylesheet" type="text/css" href="../static/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../static/css/bootstrap-theme.min.css">
+    </head>
+    <body>
+        <script type="text/javascript">
+        </script>
+        <nav class = "navbar navbar-default" role="navigation">
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a class="navbar-brand" href="#">Devernote</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Text Editor</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <h1>Devernote</h1>
+        <p>
+            cool thing these guys did this one time.
+        </p>
+    </body>
+</html>
